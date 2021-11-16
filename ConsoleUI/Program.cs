88a80aff562706c38c4a56148fba2783377d26dc
Michael Robards <mikerobards@icloud.com>
2021-11-16 18:00:10 -0500

    Add beginning methods and some logic

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,52 +1,108 @@
-﻿using System;
+﻿using BattleshipLiteLibrary;
+using BattleshipLiteLibrary.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleUI
+namespace BattleshipLite
 {
     class Program
     {
         static void Main(string[] args)
         {
+            WelcomeMessage();
 
+            PlayerInfoModel player1 = CreatePlayer("Player 1");
+            PlayerInfoModel player2 = CreatePlayer("Player 2");
+
+            Console.ReadLine();
         }
 
-        private static string RequestUserName()
+        private static void WelcomeMessage()
         {
-
+            Console.WriteLine("Welcome to Battleship Lite!");
+            Console.WriteLine("created by Mike Robards");
+            Console.WriteLine();
         }
 
-        private static string RequestShipPlacement()
+        private static PlayerInfoModel CreatePlayer(string playerTitle)
         {
+            PlayerInfoModel output = new PlayerInfoModel();
 
+            Console.WriteLine($"Player information for {playerTitle}");
+
+            // Ask the user for their name
+            output.UsersName = AskForUsersName();
+
+            // Load up the shot grid
+            GameLogic.InitializeGrid(output);
+
+            // Ask the user from their 5 ship placements
+            PlaceShips(output);
+
+            // Clear
+            Console.Clear();
+
+            return output;
         }
 
-        private static string DisplayGrid()
+        private static string AskForUsersName()
         {
+            Console.Write("What is your name: ");
+            string output = Console.ReadLine();
 
+            return output;
         }
 
-        private static string FireOnOpponent()
+        private static void PlaceShips(PlayerInfoModel model)
         {
+            do
+            {
+                Console.Write($"Where do you want to place ship number {model.ShipLocations.Count + 1}: ");
+                string location = Console.ReadLine();
 
+                bool isValidLocation = GameLogic.PlaceShip(model, location);
+
+                if (!isValidLocation)
+                {
+                    Console.WriteLine("That was not a valid location. Please try again.");
+                }
+
+            } while (model.ShipLocations.Count < 5);
         }
 
-        private static string CanShotBeTaken()
-        {
 
-        }
+        //private static string RequestShipPlacement()
+        //{
 
-        private static string DisplayScore()
-        {
+        //}
 
-        }
+        //private static string DisplayGrid()
+        //{
 
-        private static string DisplayWinner()
-        {
+        //}
 
-        }
+        //private static string FireOnOpponent()
+        //{
+
+        //}
+
+        //private static string CanShotBeTaken()
+        //{
+
+        //}
+
+        //private static string DisplayScore()
+        //{
+
+        //}
+
+        //private static string DisplayWinner()
+        //{
+
+        //}
 
     }
 }
