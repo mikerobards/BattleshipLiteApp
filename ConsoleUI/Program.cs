132e34ea880f0be6a4f2693a404a461c56790219
Michael Robards <mikerobards@icloud.com>
2021-11-16 16:41:21 -0500

    add method names

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        private static string RequestUserName()
+        {
+
+        }
+
+        private static string RequestShipPlacement()
+        {
+
+        }
+
+        private static string DisplayGrid()
+        {
+
+        }
+
+        private static string FireOnOpponent()
+        {
+
+        }
+
+        private static string CanShotBeTaken()
+        {
+
+        }
+
+        private static string DisplayScore()
+        {
+
+        }
+
+        private static string DisplayWinner()
+        {
+
+        }
+
+    }
+}
