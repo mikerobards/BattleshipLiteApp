e3f70895fac0452db44c028b890945ea128403fe
Michael Robards <mikerobards@icloud.com>
2021-11-17 17:06:06 -0500

    completed gamelogic

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -20,21 +20,71 @@ namespace BattleshipLite
 
             do
             {
-                // Display grid from activePlayer on where they fired
                 DisplayShotGrid(activePlayer);
 
-                // Ask activePlayer for a shot
-                // Determine if it is a valid shot
-                // Determine shot results
-                // Determine if the game is over
-                // If over, set activePlayer as winner
-                // Else swap positions (activePlayer to opponent)
+                RecordPlayerShot(activePlayer, opponent);
 
+                bool doesGameContinue = GameLogic.PlayerStillActive(opponent);
+
+                if (doesGameContinue == true)
+                {
+                    // Use Tuple to swap positions
+                    (activePlayer, opponent) = (opponent, activePlayer);
+                }
+                else
+                {
+                    winner = activePlayer;
+                }
 
             } while (winner == null);
 
+            IdentifyWinner(winner);
 
             Console.ReadLine();
+        }
+
+        private static void IdentifyWinner(PlayerInfoModel winner)
+        {
+            Console.WriteLine($"Congratulations to {winner.UsersName} for winning!");
+            Console.WriteLine($"{winner.UsersName} took {GameLogic.GetShotCount(winner)} shots.");
+        }
+
+        private static void RecordPlayerShot(PlayerInfoModel activePlayer, PlayerInfoModel opponent)
+        {
+            bool isValidShot = false;
+            string row = "";
+            int column = 0;
+
+            do
+            {
+                string shot = AskForShot();
+                (row, column) = GameLogic.SplitShotIntoRowAndColumn(shot);
+                isValidShot = GameLogic.ValidateShot(activePlayer, row, column);
+
+                if (!isValidShot)
+                {
+                    Console.WriteLine("Invalid shot location. Please try again.");
+                }
+            } while (!isValidShot);
+
+
+            // Determine shot results
+            bool isAHit = GameLogic.IdentifyShotResult(opponent, row, column);
+
+            // Record results
+            GameLogic.MarkShotResult(activePlayer,
+                                     row,
+                                     column,
+                                     isAHit);
+
+        }
+
+        private static string AskForShot()
+        {
+            Console.Write("Please enter your shot selection: ");
+            string output = Console.ReadLine();
+
+            return output;
         }
 
         private static void DisplayShotGrid(PlayerInfoModel activePlayer)
@@ -124,35 +174,6 @@ namespace BattleshipLite
         }
 
 
-        //private static string RequestShipPlacement()
-        //{
-
-        //}
-
-        //private static string DisplayGrid()
-        //{
-
-        //}
-
-        //private static string FireOnOpponent()
-        //{
-
-        //}
-
-        //private static string CanShotBeTaken()
-        //{
-
-        //}
-
-        //private static string DisplayScore()
-        //{
-
-        //}
-
-        //private static string DisplayWinner()
-        //{
-
-        //}
 
     }
 }
